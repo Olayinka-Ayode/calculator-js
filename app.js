8ae2d6d63d5cb7a34c4f3461898daf8ab7c56c8b
Olayinka <theolayinkayodele@gmail.com>
2023-05-07 16:35:27 +0100

    completes first edition

---
+++ app.js
@@ -0,0 +1,38 @@
+const screen = document.getElementById("screen");
+const ac = document.getElementById("AC");
+const changeSign = document.getElementById("plus-minus");
+const percent = document.getElementById("percent");
+const multiply = document.getElementById("multiply");
+const divide = document.getElementById("divide");
+const minus = document.getElementById("minus");
+const plus = document.getElementById("plus");
+const sqr = document.getElementById("sqr");
+const evalu = document.getElementById("equals");
+
+screen.value += "79"
+
+function removeC(){
+    screen.value = screen.value.slice(0, screen.value.length - 1)
+}
+
+
+function onAc(){
+    screen.value = ''
+}
+
+function percentage(){
+    screen.value = screen.value / 100
+}
+
+function showValue(e){
+    screen.value += e
+}
+
+function sqRoot(){
+    screen.value = Math.sqrt(screen.value).toFixed(3)
+}
+
+function giveAnswer(){
+    calcu = screen.value.split(" ");
+    screen.value = eval(screen.value).toFixed(3)
+}
